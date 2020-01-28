53589943ac91fdb71fe64e35fed5c75bd2212984
profgrammer <javarep4199@gmail.com>
2020-01-28 11:39:48 +0530

    add cors

--- htmlpdf.js
+++ htmlpdf.js
@@ -44,6 +44,16 @@ const app = express();
 const bp = require('body-parser');
 app.use(bp.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    if(req.method === 'OPTIONS'){
+      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+      return res.status(200).json({});
+    }
+    next();
+});  
+
 app.get('/download', (req, res) => {
     const student = req.body.student;
     ejs.renderFile("./hallticket.ejs", {student: student}, (err, data) => {
